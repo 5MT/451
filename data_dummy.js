938cd206ab88114dfe27d5bfbac5d5e6f26d4011
5MT <run5mt_ms@hotmail.com>
2020-03-28 14:17:41 +0900

    データファイル分離

---
+++ data_dummy.js
@@ -0,0 +1,41 @@
+﻿'use strict';
+const arrTasks = [
+	,["","Cat01","Subcat01","Item description","201701","201702","","",""]
+	,["","Cat01","Subcat01","Item description","201703","201703","","",""]
+
+	,["","Cat01","Subcat02","Item description","201701","201702","","",""]
+	,["","Cat01","Subcat02","Item description","201609","201702","201609","201609",""]
+	,["","Cat01","Subcat02","Item description","201702","201703","","",""]
+	,["","Cat01","Subcat02","Item description","201704","201704","","",""]
+	,["","Cat01","Subcat02","Item description","201704","201705","","",""]
+
+	,["","Cat01","Subcat03","Item description","201611","201703","201611","201612",""]
+	,["","Cat01","Subcat03","Item description","201706","201707","","",""]
+	,["","Cat01","Subcat03","Item description","201708","201709","","",""]
+
+	,["","Cat02","Subcat01","Item description","201701","201701","","",""]
+	,["","Cat02","Subcat01","Item description","201702","201702","","",""]
+	,["","Cat02","Subcat01","Item description","201702","201702","","",""]
+	,["","Cat02","Subcat02","Item description","201701","201701","","",""]
+
+	,["","Cat03","Subcat01","Item description","201611","201702","","",""]
+	,["","Cat03","Subcat01","Item description","201611","201703","","",""]
+	,["","Cat03","Subcat01","Item description","201704","201709","","",""]
+	,["","Cat03","Subcat01","Item description","201611","201709","","",""]
+];
+
+// カテゴリ集
+const arrThemes = [
+	 ["Cat01","desc","active"]
+	,["Cat02","",""]
+	,["Cat03","",""]
+	,["Cat04","desc2","inactive"]
+];
+
+// Team
+const arrTeams = [
+	 ["Cat01","desc","active","last activity"]
+	,["Cat02","desc","active","last activity"]
+	,["Cat03","desc","2015","last activity"]
+	,["Cat04","desc","2016","last activity"]
+];
